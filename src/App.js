ad4a778c07447c1b0053788edccf639583c27f47
krinsh panchal <jayveer1301@gmail.com>
2023-09-08 02:40:39 +0530

    add this new login page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './component/login';
+
+function App() {
+  return (
+    <div className="">
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      my name is krinsh
+    </div>
+  );
+}
+
+export default Login;
