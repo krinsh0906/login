4bb4f1e26d63c35a00137299fb607d114680fd83
krinsh panchal <jayveer1301@gmail.com>
2023-09-08 13:56:31 +0530

    add this loginpage

--- src/component/login.js
+++ src/component/login.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { useState } from "react";
+import { FaFacebook } from 'react-icons/fa';
+import { FaGoogle } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa'
 
-function Login() {
+
+
+
+function Login(
+  
+) {
   return (
-    <div>
-      my name is krinsh
+    <div className='main-container'>
+      <div className='main-headding'>
+        <h2> LOGIN</h2>
+      </div>
+      <div className='main-input'>
+        <div>
+          <label>
+            Email:
+          </label><br />
+          <input type='email' name='email' placeholder='Email' ></input><br />
+        </div>
+
+        <div className='space'>
+          <label>
+            Password:
+          </label><br />
+          <input className='' type='password' name='password' placeholder='password'/>
+        </div>
+        <div class="forgotpass">
+          <a href="#">Forgot password?</a>
+        </div>
+        <button type='sumbit'> LOGIN </button>
+        <div class="signup space" >Don't Have account?
+          <a href="#">Signup now</a>
+
+        </div>
+
+
+      </div>
+
     </div>
+
+
   );
 }
 
